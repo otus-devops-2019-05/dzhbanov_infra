d0896693d3283ddf1db9e834ce085746764cb00a
dzhbanov <zhbanov@vtb.ru>
2019-04-12 10:42:58 +0300

    added main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+terraform {
+# Версия terraform
+required_version = "0.11.11"
+}
+provider "google" {
+# Версия провайдера
+version = "2.0.0"
+# ID проекта
+project = "infra-235619"
+region = "europe-west-1"
+}
