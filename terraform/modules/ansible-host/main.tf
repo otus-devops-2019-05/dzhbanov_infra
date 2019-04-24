0cd384e55854b5e7a8b15684bb51919372625c5d
dzhbanov <zhbanov@vtb.ru>
2019-04-24 22:24:37 +0300

    removed static ip from ans host

--- terraform/modules/ansible-host/main.tf
+++ terraform/modules/ansible-host/main.tf
@@ -4,9 +4,11 @@ name = "ansible-host"
 machine_type = "g1-small"
 zone = "${var.zone}"
 tags = ["ansible-host"]
+
 boot_disk {
 initialize_params { image = "${var.ansible-host_disk_image}" }
 }
+
 network_interface {
 network = "default"
 access_config = {
@@ -17,7 +19,9 @@ metadata {
 ssh-keys = "appuser:${file(var.public_key_path)}"
 }
 }
-resource "google_compute_address" "app_ip" { name = "ansible-host-ip" }
+
+
+
 
 
 
