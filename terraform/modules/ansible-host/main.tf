59082700add5dc084c0687097564e92e1b0a2fbc
dzhbanov <zhbanov@vtb.ru>
2019-04-26 18:33:30 +0300

    correct copy key

--- terraform/modules/ansible-host/main.tf
+++ terraform/modules/ansible-host/main.tf
@@ -27,12 +27,6 @@ provisioner "remote-exec" {
 script = "../files/ansible_install.sh"
 }
 
-# создаю файл для ключа
-provisioner "remote-exec" {
-inline = "sudo touch ~/.ssh/appuser"
-inline = "sudo chown appuser:appuser ~/.ssh/appuser"
-inline = "echo 'finished creating files!!!!!!!!!!!!'"
-  }
 
 #копирую ключ 
 provisioner "file" {
